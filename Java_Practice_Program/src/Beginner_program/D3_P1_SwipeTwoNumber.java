2af864679ae788c5d8060e4f777b4b786d871317
ssadar1432 <ssadar64@gmail.com>
2024-11-30 16:05:52 +0530

    Code completed swipe two number Day 3

--- Java_Practice_Program/src/Beginner_program/D3_P1_SwipeTwoNumber.java
+++ Java_Practice_Program/src/Beginner_program/D3_P1_SwipeTwoNumber.java
@@ -3,6 +3,17 @@ package Beginner_program;
 public class D3_P1_SwipeTwoNumber {
     public static void main(String[] args) {
 
+        int num1=10;
+
+        int num2=20;
+        System.out.println("Before swapping:"+"number 1="+num1+",number 2="+num2);
+        num1=num1+num2; //30
+        num2=num1-num2;
+        num1=num1-num2;
+
+        System.out.println("After swapping:"+"number 1="+num1+",number 2="+num2);
+
+
     }
 
 }
